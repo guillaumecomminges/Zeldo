862346de37561be70ba3b025e4988ce301caea29
Guillaume <guiguicomm@hotmail.fr>
2020-01-27 15:29:40 +0100

    ajout dialogue victoire + correction worldmap

---
+++ Assets/Dialogues/Victory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+	[SerializeField]
+	Dialogue m_winMessage = null;
+
+	[SerializeField]
+	private DialogBox m_dialogBox = null;
+
+	private void Update()
+	{
+		if(transform.gameObject.GetComponent<Entity>().GetLife() <= 0 )
+		{
+			VictoryClaim();
+		}
+	}
+
+	private void VictoryClaim()
+	{
+		m_dialogBox.DisplayDialog(m_winMessage);
+	}
+}
